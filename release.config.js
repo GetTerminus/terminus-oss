1e2e68b45b2851cd5e6dd03348b73c412034978b
Benjamin Charity <ben@benjamincharity.com>
2020-06-03 16:35:33 -0400

    WIP: try new release config

--- release.config.js
+++ release.config.js
@@ -1,55 +1,70 @@
 module.exports = {
   extends: ['semantic-release-monorepo'],
   branches: ['master', 'tokens-release-flow-v2'],
-  plugins: [
-    [
-      '@semantic-release/release-notes-generator',
+  monorepo: {
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      [
+        '@semantic-release/release-notes-generator',
+        {
+          parserOpts: {
+            noteKeywords: [
+              'BREAKING CHANGE',
+              'BREAKING CHANGES',
+              'BREAKING',
+            ],
+          },
+          writerOpts: {
+            commitsSort: [
+              'subject',
+              'scope',
+            ],
+          },
+        },
+      ],
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: 'CHANGELOG.md',
+        },
+      ],
+      '@semantic-release/npm',
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md', 'package.json', 'packages/*/*/package.json'],
+          // eslint-disable-next-line no-template-curly-in-string
+          message: 'Build: ${nextRelease.gitTag} [skip ci]',
+        },
+      ],
+      '@semantic-release/github',
+    ],
+    verifyConditions: [
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/git',
+      // '@semantic-release/github',
+    ],
+    prepare: [
       {
-        parserOpts: {
-          noteKeywords: [
-            'BREAKING CHANGE',
-            'BREAKING CHANGES',
-            'BREAKING',
-          ],
-        },
-        writerOpts: {
-          commitsSort: [
-            'subject',
-            'scope',
-          ],
-        },
+        path: '@semantic-release/changelog',
+        changelogFile: 'CHANGELOG.md',
+      },
+      {
+        path: '@semantic-release/git',
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'Add ${nextRelease.version} release notes [skip ci]',
       },
     ],
-    '@semantic-release/git',
-  ],
-  verifyConditions: [
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/git',
-    '@semantic-release/github',
-  ],
-  prepare: [
-    {
-      path: '@semantic-release/changelog',
-      changelogFile: 'CHANGELOG.md',
-    },
-    {
-      path: '@semantic-release/git',
-      // eslint-disable-next-line no-template-curly-in-string
-      message: 'Add ${nextRelease.version} release notes [skip ci]',
-    },
-  ],
-  publish: [
-    {
-      path: '@semantic-release/npm',
-      // pkgRoot: 'dist/library',
-    },
-    {
-      path: '@semantic-release/github',
-      assets: [
-        'CHANGELOG.md',
-      ],
-      npmPublish: false,
-    },
-  ],
+    publish: [
+      '@semantic-release/npm',
+      // {
+      //   path: '@semantic-release/github',
+      //   assets: [
+      //     'CHANGELOG.md',
+      //   ],
+      //   npmPublish: false,
+      // },
+    ],
+  },
 }
