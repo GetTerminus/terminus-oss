207e45fd029b93566db24e79a9eae84808949c50
Benjamin Charity <ben@benjamincharity.com>
2020-06-08 08:41:53 -0400

    fix(Chip): cleanup + trigger release

--- libs/ui/chip/src/lib/ui-chip.module.spec.ts
+++ libs/ui/chip/src/lib/ui-chip.module.spec.ts
@@ -1,5 +1,7 @@
-
-import { async, TestBed } from '@angular/core/testing';
+import {
+  async,
+  TestBed,
+} from '@angular/core/testing';
 
 import { TsChipModule } from './ui-chip.module';
 
@@ -7,8 +9,7 @@ describe(`UiChipModule`, () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [TsChipModule],
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   test(`should exist`, () => {
--- libs/ui/chip/src/lib/ui-chip.module.ts
+++ libs/ui/chip/src/lib/ui-chip.module.ts
@@ -2,13 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
 
-
 import { TsIconModule } from '@terminus/ui-icon';
 
 import { TsChipBadgeDirective } from './badge/chip-badge.directive';
 import { TsChipComponent } from './chip/chip.component';
 import { TsChipCollectionComponent } from './collection/chip-collection.component';
-
 
 export * from './chip/chip.component';
 export * from './collection/chip-collection.component';
@@ -29,4 +27,4 @@ const EXPORTED_DECLARATIONS = [
   declarations: EXPORTED_DECLARATIONS,
   exports: EXPORTED_DECLARATIONS,
 })
-export class TsChipModule { }
+export class TsChipModule {}
