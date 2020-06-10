ff26b806bb599401f006996be5b567a378e68ef3
Benjamin Charity <ben@benjamincharity.com>
2020-06-10 13:00:51 -0400

    fix: bump all internal dependencies

--- libs/ui/autofocus/src/lib/ui-autofocus.module.spec.ts
+++ libs/ui/autofocus/src/lib/ui-autofocus.module.spec.ts
@@ -1,5 +1,7 @@
-
-import { async, TestBed } from '@angular/core/testing';
+import {
+  async,
+  TestBed,
+} from '@angular/core/testing';
 
 import { TsAutofocusModule } from './ui-autofocus.module';
 
@@ -7,8 +9,7 @@ describe('UiAutofocusModule', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [TsAutofocusModule],
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   test(`should exist`, () => {
