0208b98b99426324e1551c850c2a39ac25c9df9f
Benjamin Charity <ben@benjamincharity.com>
2020-06-10 08:44:54 -0400

    chore(CI): update lint staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -6,32 +6,20 @@ const IGNORE = [
   'node_modules/**',
   'package.json',
   'yarn.lock',
+  '.jest-cache/**',
+  'cypress/**',
 ];
 
 module.exports = {
-  // Target all TS & JS files
-  '**/*.{js,ts}': filenames => [
-    `eslint --fix ${removeDirectories(filenames).map(f => `"${f}"`).join(' ')}`,
-  ],
+  '**/*.{ts,js,json}': files => {
+    const match = micromatch.not(files, IGNORE);
+    return [
+      `eslint --fix ${match.join(' ')}`,
+    ]
+  },
 
   // Target library SCSS files
   'libs/**/!(*.spec).scss': files => [
     `stylelint --fix ${files.map(f => `"${f}"`).join(' ')}`,
   ],
 };
-
-
-/**
- * Function to remove any testing or demo files and return an array containing all file names
- *
- * @param files
- * @returns Array
- */
-const removeDirectories = files => micromatch.not(files, [
-  ...IGNORE,
-  '**/testing/**',
-  '**/demo/**',
-  '**/cypress/**',
-  '**/vr/**',
-]);
-
