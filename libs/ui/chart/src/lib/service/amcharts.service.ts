110038a76acef38dda3d2cdc4478c1690048424a
Benjamin Charity <ben@benjamincharity.com>
2020-06-10 07:16:38 -0400

    fix(Chart): cleanup and trigger release

--- libs/ui/chart/src/lib/service/amcharts.service.ts
+++ libs/ui/chart/src/lib/service/amcharts.service.ts
@@ -4,7 +4,6 @@ import {
   InjectionToken,
 } from '@angular/core';
 
-
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export interface TsAmChartsToken {
   core: any;
@@ -19,16 +18,13 @@ export interface TsAmChartsToken {
  */
 export const TS_AMCHARTS_TOKEN = new InjectionToken<TsAmChartsToken>('amCharts');
 
-
 /**
  * Expose amCharts via a service
  */
 @Injectable({ providedIn: 'root' })
 export class TsAmChartsService {
 
-  constructor(
-    @Inject(TS_AMCHARTS_TOKEN) public amCharts: TsAmChartsToken,
-  ) {
+  constructor(@Inject(TS_AMCHARTS_TOKEN) public amCharts: TsAmChartsToken) {
     // istanbul ignore else
     if (amCharts && amCharts.themes && amCharts.themes.length > 0) {
       for (const theme of amCharts.themes) {
