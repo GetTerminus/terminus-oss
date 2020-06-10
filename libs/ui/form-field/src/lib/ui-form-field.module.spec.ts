9a6a4bbd410adc6eab94891164ae1ab8804c1350
Benjamin Charity <ben@benjamincharity.com>
2020-06-10 07:45:48 -0400

    fix(FormField): trigger release

--- libs/ui/form-field/src/lib/ui-form-field.module.spec.ts
+++ libs/ui/form-field/src/lib/ui-form-field.module.spec.ts
@@ -1,5 +1,7 @@
-
-import { async, TestBed } from '@angular/core/testing';
+import {
+  async,
+  TestBed,
+} from '@angular/core/testing';
 
 import { TsFormFieldModule } from './ui-form-field.module';
 
@@ -7,8 +9,7 @@ describe(`TsFormFieldModule`, () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [TsFormFieldModule],
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   test(`should exist`, () => {
