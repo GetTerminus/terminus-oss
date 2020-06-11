8d6d61e3c8e18b688528e60be53200a89ae95037
Benjamin Charity <ben@benjamincharity.com>
2020-06-11 11:20:12 -0400

    fix(Spacing): trigger release

--- libs/ui/spacing/src/lib/ui-spacing.module.spec.ts
+++ libs/ui/spacing/src/lib/ui-spacing.module.spec.ts
@@ -1,5 +1,7 @@
-
-import { async, TestBed } from '@angular/core/testing';
+import {
+  async,
+  TestBed,
+} from '@angular/core/testing';
 
 import { TsSpacingModule } from './ui-spacing.module';
 
@@ -7,8 +9,7 @@ describe(`TsSpacingModule`, () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [TsSpacingModule],
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   test(`should exist`, () => {
