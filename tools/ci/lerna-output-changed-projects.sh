e55e14da6bb5671c8bf9eadce53620977b468ff8
Benjamin Charity <ben@benjamincharity.com>
2020-06-11 15:35:56 -0400

    chore(CI): move track into lerna

--- tools/ci/lerna-output-changed-projects.sh
+++ tools/ci/lerna-output-changed-projects.sh
@@ -7,6 +7,11 @@ LERNA_OUTPUT=$(npx lerna changed --include-merged-tags --loglevel=silent --json 
 if [[ -n "$LERNA_OUTPUT" ]]; then
   echo "Changed projects found. Piping to file.."
   echo "$LERNA_OUTPUT" > CHANGED.json
+
+  export CHANGED_PROJECTS="$(node json-to-projects-list.js)"
+  echo "Building: $CHANGED_PROJECTS"
+  yarn run build:changed
 else
   echo "No changed projects found."
 fi
+
