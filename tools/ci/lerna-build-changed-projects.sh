106ac90b3d76bdd4871cb58531e58ab8949ebb8e
Benjamin Charity <ben@benjamincharity.com>
2020-06-12 08:43:48 -0400

    chore(CI): build all projects until up to date

--- tools/ci/lerna-build-changed-projects.sh
+++ tools/ci/lerna-build-changed-projects.sh
@@ -3,14 +3,18 @@
 . ~/.bashrc
 
 LERNA_OUTPUT=$(npx lerna changed --include-merged-tags --loglevel=silent --json || :)
+echo "Lerna changed check: $LERNA_OUTPUT"
 
 if [[ -n "$LERNA_OUTPUT" ]]; then
   echo "Changed projects found. Piping to file.."
   echo "$LERNA_OUTPUT" > CHANGED.json
+  echo "Cat changed.json: "
+  cat CHANGED.json
 
-  export CHANGED_PROJECTS="$(node json-to-projects-list.js)"
-  echo "Building: $CHANGED_PROJECTS"
-  yarn run build:changed
+#  export CHANGED_PROJECTS="$(node json-to-projects-list.js)"
+#  echo "Building: $CHANGED_PROJECTS"
+  echo "TEMPORARY: Building ALL projects"
+  yarn run build:all
 else
   echo "No changed projects found."
 fi
