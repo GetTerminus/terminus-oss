66546c9e75cb299c267385fc6e0c647c57334564
Benjamin Charity <ben@benjamincharity.com>
2020-06-15 08:32:11 -0400

    chore(CI): fix "quoted commits" messages w/quotes again

--- tools/ci/set-should-run-flag.sh
+++ tools/ci/set-should-run-flag.sh
@@ -3,9 +3,17 @@
 . ~/.bashrc
 
 OG_MESSAGE=$1
+#OG_MESSAGE="revert(CI): fix \"quoted commits\" messages w/quotes"
 echo "Original commit Message: $OG_MESSAGE"
 
-MESSAGE=$(echo "$OG_MESSAGE" | sed -E "s/\\\"/\\'/g")
+
+#temp="${OG_MESSAGE%\"}"
+#temp="${temp#\"}"
+#echo "TEMP: $temp"
+#echo "OG: "
+#echo "$OG_MESSAGE" | tr -d '"'
+
+MESSAGE=$(echo "$OG_MESSAGE" | tr -d '"')
 echo "Cleaned commit message: $MESSAGE"
 
 if [[ "$MESSAGE" =~ (skip\ ci|ci\ skip) ]]; then
