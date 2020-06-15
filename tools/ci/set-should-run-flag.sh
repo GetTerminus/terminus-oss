5763eb8686944b220a3a5f29cd62a881cc39684b
Benjamin Charity <ben@benjamincharity.com>
2020-06-15 08:35:25 -0400

    fix(CI): clean messages [skip ci]

--- tools/ci/set-should-run-flag.sh
+++ tools/ci/set-should-run-flag.sh
@@ -3,15 +3,7 @@
 . ~/.bashrc
 
 OG_MESSAGE=$1
-#OG_MESSAGE="revert(CI): fix \"quoted commits\" messages w/quotes"
 echo "Original commit Message: $OG_MESSAGE"
-
-
-#temp="${OG_MESSAGE%\"}"
-#temp="${temp#\"}"
-#echo "TEMP: $temp"
-#echo "OG: "
-#echo "$OG_MESSAGE" | tr -d '"'
 
 MESSAGE=$(echo "$OG_MESSAGE" | tr -d '"')
 echo "Cleaned commit message: $MESSAGE"
