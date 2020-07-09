e0c8b8e9065ac584d6f1908981aaf29b5ae17118
Benjamin Charity <ben@benjamincharity.com>
2020-07-09 15:00:32 -0400

    feat(DesignTokens): add all existing typography styles

---
+++ libs/design-tokens/src/lib/base/typography/compound.js
@@ -0,0 +1,43 @@
+/* eslint-disable max-len */
+module.exports = {
+  typography: {
+    compound: {
+      display: {
+        value: `500 {typography.size.700.value} / {typography.lineHeight.600.value} {typography.stack.base.roboto.value}`,
+      },
+      display2: {
+        value: `500 45px / {typography.lineHeight.800.value} {typography.stack.base.roboto.value}`,
+      },
+      display3: {
+        value: `500 {typography.size.900.value} / {typography.lineHeight.700.value} {typography.stack.base.roboto.value}`,
+      },
+      display4: {
+        value: `400 {typography.size.1000.value} / {typography.lineHeight.1000.value} {typography.stack.base.roboto.value}`,
+      },
+      body: {
+        value: `400 {typography.size.300.value} / {typography.lineHeight.300.value} {typography.stack.base.roboto.value}`,
+      },
+      body2: {
+        value: `500 {typography.size.300.value} / {typography.lineHeight.300.value} {typography.stack.base.roboto.value}`,
+      },
+      headline: {
+        value: `500 {typography.size.600.value} / {typography.lineHeight.500.value} {typography.stack.base.roboto.value}`,
+      },
+      title: {
+        value: `{typography.size.500.value} / 1em {typography.stack.base.roboto.value}`,
+      },
+      subhead: {
+        value: `500 {typography.size.400.value} / {typography.lineHeight.400.value} {typography.stack.base.roboto.value}`,
+      },
+      caption: {
+        value: `{typography.size.200.value} / {typography.lineHeight.300.value} {typography.stack.base.roboto.value}`,
+      },
+      fixedWidth: {
+        value: `{typography.size.300.value} / {typography.lineHeight.300.value} {typography.stack.base.monospaced.value}`,
+      },
+      hint: {
+        value: `400 75% / 1.5 {typography.stack.base.roboto.value}`,
+      },
+    },
+  },
+};
