6313c0a616d133337b582f17d9eca4893015df2e
Benjamin Charity <ben@benjamincharity.com>
2020-07-09 10:22:56 -0400

    chore(CI): update demos deploy to work locally

--- tools/ci/demos-deploy.sh
+++ tools/ci/demos-deploy.sh
@@ -7,24 +7,34 @@
 # shellcheck disable=SC1090
 . ~/.bashrc
 
-# Likely starting at `/home/runner/work/terminus-oss/terminus-oss/t-oss` so we need to go back one
-cd ../
+echo "Running on CI: $CI"
 
 echo "CURRENT PATH:"
 pwd
 
 # Enter the demos repo
-cd ui-demos-release || exit
-
-# Remove old demo files
-echo "Deleting old demo files:"
-find . -regextype posix-extended -regex '.*\.(html|css|js)' -type f -delete
+cd ../ui-demos-release || exit
+#if [[ "$CI" = true ]]; then
+#fi
 
 echo "CURRENT PATH:"
 pwd
 
+# Remove old demo files
+echo "Deleting old demo files:"
+# NOTE: Mac/Linux support different regex options
+if [[ "$CI" = true ]]; then
+  find . -regextype posix-extended -regex '.*\.(html|css|js)' -type f -delete
+else
+  find -E . -regex '.*\.(html|css|js)' -type f -not -path "./node_modules/*" -delete
+fi
+
 # Move new files into the cloned repo
-mv -f -v ../t-oss/dist/apps/showcase-ui/* .
+if [[ "$CI" = true ]]; then
+  mv -f -v ../t-oss/dist/apps/showcase-ui/* .
+else
+  mv -f -v ../terminus-oss/dist/apps/showcase-ui/* .
+fi
 
 # Clone the index.html file as 404.html to support deep linking
 cp index.html 404.html
@@ -39,6 +49,10 @@ git add .
 git commit -m "Update via CI release"
 
 # Push quietly to prevent showing the token in log
-git push -q https://"${GH_TOKEN}"@github.com/GetTerminus/ui-demos-release.git master
+git push -f -q https://"${GH_TOKEN}"@github.com/GetTerminus/ui-demos-release.git master
 
-cd - || exit
+if [[ "$CI" = true ]]; then
+  cd ../t-oss || exit
+else
+  cd ../terminus-oss || exit
+fi
