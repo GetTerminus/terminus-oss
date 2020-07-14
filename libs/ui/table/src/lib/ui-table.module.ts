887ffaff52cc1017adb558e0fa8408e2ec3aaf7e
Benjamin Charity <ben@benjamincharity.com>
2020-07-14 11:35:45 -0400

    fix(Table): use css custom properties

--- libs/ui/table/src/lib/ui-table.module.ts
+++ libs/ui/table/src/lib/ui-table.module.ts
@@ -2,7 +2,6 @@ import { CdkTableModule } from '@angular/cdk/table';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-
 import { TsPaginatorModule } from '@terminus/ui-paginator';
 import { TsSortModule } from '@terminus/ui-sort';
 
@@ -25,13 +24,11 @@ import {
 } from './row/row';
 import { TsTableComponent } from './table/table.component';
 
-
 export * from './data-source/table-data-source';
 export * from './cell/cell';
 export * from './column/column';
 export * from './row/row';
 export * from './table/table.component';
-
 
 // NOTE: Moving declarations/exports items into shared array breaks documentation generation.
 @NgModule({
