e2e607d45e1da58a471636be70dd7e3f9eb26a04
Benjamin Charity <ben@benjamincharity.com>
2020-07-14 12:10:56 -0400

    fix(Confirmation): use css custom properties
    ISSUES CLOSED: #110

--- libs/ui/confirmation/src/index.ts
+++ libs/ui/confirmation/src/index.ts
@@ -1,2 +1,1 @@
-
 export * from './lib/ui-confirmation.module';
--- libs/ui/confirmation/src/lib/ui-confirmation.module.ts
+++ libs/ui/confirmation/src/lib/ui-confirmation.module.ts
@@ -3,16 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-
 import { TsButtonModule } from '@terminus/ui-button';
 
 import { TsConfirmationDirective } from './confirmation/confirmation.directive';
 import { TsConfirmationOverlayComponent } from './overlay/confirmation-overlay.component';
 
-
 export * from './overlay/confirmation-overlay.component';
 export * from './confirmation/confirmation.directive';
-
 
 @NgModule({
   imports: [
