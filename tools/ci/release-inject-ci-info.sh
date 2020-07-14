ddb1b772c228184d5b12da6558af890cd3033126
Benjamin Charity <ben@benjamincharity.com>
2020-07-14 15:16:16 -0400

    chore(CI): revert path change

--- tools/ci/release-inject-ci-info.sh
+++ tools/ci/release-inject-ci-info.sh
@@ -11,11 +11,13 @@ NOW=$(date)
 echo "Build date/time: $NOW"
 echo "Run ID: $GITHUB_RUN_ID"
 echo "SHA: $GITHUB_SHA"
+echo "Current path:"
+pwd
 
 # Replace the placeholders with the correct values
-grep -rl $TIME_PLACEHOLDER 't-oss/dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$TIME_PLACEHOLDER'|'"$NOW"'|g'
-grep -rl $REF_PLACEHOLDER 't-oss/dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$REF_PLACEHOLDER'|'"$GITHUB_RUN_ID"'|g'
-grep -rl $SHA_PLACEHOLDER 't-oss/dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$SHA_PLACEHOLDER'|'"$GITHUB_SHA"'|g'
+grep -rl $TIME_PLACEHOLDER 'dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$TIME_PLACEHOLDER'|'"$NOW"'|g'
+grep -rl $REF_PLACEHOLDER 'dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$REF_PLACEHOLDER'|'"$GITHUB_RUN_ID"'|g'
+grep -rl $SHA_PLACEHOLDER 'dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$SHA_PLACEHOLDER'|'"$GITHUB_SHA"'|g'
 
 # Delete edit reference files left over from the sed replacement
 # NOTE: The force flag is required so that the build isn't cancelled if these files don't exist
