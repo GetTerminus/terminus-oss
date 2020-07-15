9452f49410437dbc627ea7e5339e28311bb41649
Benjamin Charity <ben@benjamincharity.com>
2020-07-15 08:41:26 -0400

    feat(DesignTokens): update base typography stack
    Updating fallback typefaces for LPS project
    
    BREAKING CHANGE:
    Base font stack fallback typefaces have been updated.
    
    ISSUES CLOSED: #22

--- libs/design-tokens/src/lib/base/typography/base.js
+++ libs/design-tokens/src/lib/base/typography/base.js
@@ -8,9 +8,10 @@ module.exports = {
     },
     stack: {
       base: {
-        roboto: { value: '{typography.family.base.roboto.value}, \'helvetica neue\', helvetica, arial, sans-serif' },
+        roboto: { value: '{typography.family.base.roboto.value}, -apple-system, BlinkMacSystemFont, \'Segoe UI\', Ubuntu, \'Droid'
+            + ' Sans\', \'Helvetica Neue\', sans-serif' },
         // HACK: 'monospace' should be declared twice. See: https://stackoverflow.com/a/36540436/722367
-        monospaced: { value: '{typography.family.base.monospaced.value}, \'Open Sans\', monaco,  monospace, monospace' },
+        monospaced: { value: '{typography.family.base.monospaced.value}, \'Open Sans\', monaco, monospace, monospace' },
       },
     },
   },
