14266ea6711a68c5d0d5a7597f907edbbf5f5d53
Benjamin Charity <ben@benjamincharity.com>
2020-07-15 09:20:46 -0400

    chore(CI): attempt cleanup between each replace

--- tools/ci/release-inject-ci-info.sh
+++ tools/ci/release-inject-ci-info.sh
@@ -15,10 +15,17 @@ echo "Current path:"
 pwd
 
 # Replace the placeholders with the correct values
+echo "Inject build time.."
 grep -rl $TIME_PLACEHOLDER 'dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$TIME_PLACEHOLDER'|'"$NOW"'|g'
-grep -rl $REF_PLACEHOLDER 'dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$REF_PLACEHOLDER'|'"$GITHUB_RUN_ID"'|g'
-grep -rl $SHA_PLACEHOLDER 'dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$SHA_PLACEHOLDER'|'"$GITHUB_SHA"'|g'
-
 # Delete edit reference files left over from the sed replacement
 # NOTE: The force flag is required so that the build isn't cancelled if these files don't exist
 rm -f dist/apps/showcase-ui/*-e
+
+echo "Inject run ID.."
+grep -rl $REF_PLACEHOLDER 'dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$REF_PLACEHOLDER'|'"$GITHUB_RUN_ID"'|g'
+rm -f dist/apps/showcase-ui/*-e
+
+echo "Inject SHA.."
+grep -rl $SHA_PLACEHOLDER 'dist/apps/showcase-ui' | xargs sed -i'' -e 's|'$SHA_PLACEHOLDER'|'"$GITHUB_SHA"'|g'
+rm -f dist/apps/showcase-ui/*-e
+m -f dist/apps/showcase-ui/*-e
