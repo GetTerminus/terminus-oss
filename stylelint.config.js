7198362206a701281981d5db3f6392edc7cc60ff
Benjamin Charity <ben@benjamincharity.com>
2020-07-17 09:17:03 -0400

    chore: remove indistinguishable colors rule
    Causing too many issues during LPS

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,43 +2,9 @@ module.exports = {
   extends: '@terminus/stylelint-config-frontend',
   rules: {
     'no-eol-whitespace': [true, { ignore: 'empty-lines' }],
+    // This was causing too many warnings once product started the LPS project
+    'plugin/stylelint-no-indistinguishable-colors': null,
     // TODO: remove once stylelint config is updated
-    'scale-unlimited/declaration-strict-value': [
-      [
-        'box-shadow',
-        '/^((.+-)?(color))$/',
-        '/font/',
-        '/^(margin|padding)(-top|-left|-bottom|-right)?/',
-        'z-index',
-      ],
-      {
-        disableFix: true,
-        ignoreFunctions: true,
-        ignoreKeywords: {
-          '': [
-            'auto',
-            'currentColor',
-            'inherit',
-            'none',
-          ],
-          '/^((.+-)?(color))$/': [
-            'currentColor',
-            'inherit',
-            'transparent',
-          ],
-          '/^(margin|padding)(-top|-left|-bottom|-right)?/': [
-            '0',
-            'auto',
-            'none',
-            'unset',
-          ],
-          'z-index': [
-            -1,
-            1,
-            'auto',
-          ],
-        },
-      },
-    ],
+    'scale-unlimited/declaration-strict-value': null,
   },
 };
