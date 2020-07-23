019bc2ae81b135dc5c8d185207df3c895210e696
Benjamin Charity <ben@benjamincharity.com>
2020-07-23 14:25:58 -0400

    chore(ShowcaseUI): better logo display styles

--- apps/showcase-ui/src/app/components/logo/logo.component.ts
+++ apps/showcase-ui/src/app/components/logo/logo.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'demo-logo',
   templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.scss'],
 })
-export class LogoComponent {
-}
+export class LogoComponent {}
