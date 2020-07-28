701f11aaabbe6c60c73fbfd554b2884871acd996
Benjamin Charity <ben@benjamincharity.com>
2020-07-28 09:33:15 -0400

    chore: add stylelint rules back temporarily

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: '@terminus/stylelint-config-frontend' };
+module.exports = {
+  extends: '@terminus/stylelint-config-frontend',
+  rules: {
+    // TODO: remove once stylelint config is updated
+    'plugin/stylelint-no-indistinguishable-colors': null,
+    'scale-unlimited/declaration-strict-value': null,
+  },
+};
