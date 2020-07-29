44642459ce66173d29fa289c5ef934f9766ea570
Wendy <atlwendy@gmail.com>
2020-07-29 13:08:40 -0400

    chore(TsTooltip): test helper functions should not rely on special test host content

--- libs/ui/tooltip/testing/src/test-helpers.ts
+++ libs/ui/tooltip/testing/src/test-helpers.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { TsTooltipComponent } from '@terminus/ui-tooltip';
+import { TsUILibraryError } from '@terminus/ui-utilities';
 
 /**
  * Helper function to display the tooltip
@@ -6,8 +10,23 @@ import { ComponentFixture } from '@angular/core/testing';
  * @param fixture - the fixture with a tooltip
  */
 export function getTooltipText(fixture: ComponentFixture<any>): string | null {
-  fixture.componentInstance.tooltipComponent.showTooltip();
+  const tooltipComponent = getTooltipComponent(fixture);
+  tooltipComponent.showTooltip();
   fixture.detectChanges();
   const matTooltipEl = document.querySelector('.mat-tooltip');
   return matTooltipEl.textContent;
+}
+
+/**
+ * Get tooltip component
+ *
+ * @param fixture - the fixture with a tooltip
+ * @returns tooltip component
+ */
+export function getTooltipComponent(fixture): TsTooltipComponent {
+  const debugElement = fixture.debugElement.queryAll(By.css('ts-tooltip'));
+  if (!debugElement) {
+    throw new TsUILibraryError(`Tooltip fixture did not find any tooltip debug element`);
+  }
+  return debugElement[0].componentInstance;
 }
