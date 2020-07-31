bdbd6ee799b1c866a0a65e56ca89673bb6b2b0fa
Benjamin Charity <ben@benjamincharity.com>
2020-07-31 08:44:44 -0400

    chore: remove outdated stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   extends: '@terminus/stylelint-config-frontend',
-  rules: {
-    // TODO: remove once stylelint config is updated
-    'plugin/stylelint-no-indistinguishable-colors': null,
-    'scale-unlimited/declaration-strict-value': null,
-  },
+  rules: {},
 };
