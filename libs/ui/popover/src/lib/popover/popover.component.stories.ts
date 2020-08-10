785112a788a86e8435af8350d371cb8c64f7a70e
Wendy <atlwendy@gmail.com>
2020-08-10 12:14:22 -0400

    chore: storybook/chromatic setting updates.

--- libs/ui/popover/src/lib/popover/popover.component.stories.ts
+++ libs/ui/popover/src/lib/popover/popover.component.stories.ts
@@ -14,31 +14,7 @@ export default {
   decorators: [withKnobs],
 };
 
-export const hoverTriggerWithPosition = () => ({
-  moduleMetadata: { imports: [TsPopoverModule] },
-  component: TsPopoverComponent,
-  template: `
-    <div>
-      <button
-        tsPopoverTrigger="popper2"
-        [popover]="popper2"
-        [position]="position"
-        popoverTrigger="hover"
-        style="margin: 200px 250px;"
-      >Hover over!</button>
-    </div>
-
-    <ts-popover #popper2>
-      <h1>My Title</h1>
-      <p>Other random content.</p>
-    </ts-popover>
-  `,
-  props: {
-    position: select('position', ['top', 'bottom', 'left', 'right'], 'right'),
-  },
-});
-
-export const clickTriggerWithPosition = () => ({
+export const popoverComponent = () => ({
   moduleMetadata: { imports: [TsPopoverModule] },
   component: TsPopoverComponent,
   template: `
@@ -46,6 +22,7 @@ export const clickTriggerWithPosition = () => ({
       <button
         tsPopoverTrigger="popper1"
         [position]="position"
+        [popoverTrigger]="popoverTrigger"
         [popover]="popper1"
         style="margin: 200px 250px;"
       >Click me!</button>
@@ -58,5 +35,6 @@ export const clickTriggerWithPosition = () => ({
   `,
   props: {
     position: select('position', ['top', 'bottom', 'left', 'right'], 'right'),
+    popoverTrigger: select('popoverTrigger', ['click', 'hover'], 'click'),
   },
 });
---
+++ stories/ui/storybook/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  addons: [
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-notes/register',
+    '@storybook/addon-storysource',
+    'storybook-addon-paddings',
+  ],
+}
