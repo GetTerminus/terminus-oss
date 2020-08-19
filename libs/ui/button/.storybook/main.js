4428f95260ffdaf4ead1984650cc00b19b40df75
Benjamin Charity <ben@benjamincharity.com>
2020-08-19 15:11:28 -0400

    chore(Button): set up initial states story

--- libs/ui/button/.storybook/main.js
+++ libs/ui/button/.storybook/main.js
@@ -1,3 +1,28 @@
 const globalConfig = require('../../../../stories/ui/storybook/.storybook/main');
 
-module.exports = { ...globalConfig };
+const customConfig = {
+  presets: [
+    {
+      name:'@ergosign/storybook-addon-pseudo-states-angular/preset-postcss',
+      options: {
+        postCssLoaderPseudoClassesPluginOptions: {
+          blacklist: [':enabled', ':not'],
+        },
+      },
+    },
+  ],
+  addons: [
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-notes/register',
+    '@storybook/addon-storysource',
+    'storybook-addon-paddings',
+    '@ergosign/storybook-addon-pseudo-states-angular/register',
+  ],
+}
+
+module.exports = {
+  ...globalConfig,
+  ...customConfig,
+};
