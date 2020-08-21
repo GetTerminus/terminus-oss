cb8e0fec5c20109d3a4b9fdf74cc6f697069d18a
Benjamin Charity <ben@benjamincharity.com>
2020-08-21 08:01:00 -0400

    chore(ExpansionPanel): remove unneeded lint disable comment

--- libs/ui/expansion-panel/src/lib/expansion-panel.stories.ts
+++ libs/ui/expansion-panel/src/lib/expansion-panel.stories.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @angular-eslint/prefer-on-push-component-change-detection */
 import { Component } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
