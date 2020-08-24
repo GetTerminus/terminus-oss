7f6c7f61e409b1627ae2634dc2568422323ee0f5
Benjamin Charity <ben@benjamincharity.com>
2020-08-24 11:07:30 -0400

    chore: temporarily add new eslint config
    This will soon be removed when the next version of our eslint config
    plugin is released.

--- .eslintrc.js
+++ .eslintrc.js
@@ -114,16 +114,38 @@ module.exports = {
       },
     },
 
-    // Story files
+    // TEMPORARY: ESLint release coming that will remove the need for this
     {
       files: [
         '**/*.stories.ts',
         'stories/**/*.ts',
       ],
       rules: {
-        '@angular-eslint/prefer-on-push-component-change-detection': DISABLED,
+        '@angular-eslint/component-class-suffix': DISABLED,
+        '@angular-eslint/component-max-inline-declarations': DISABLED,
         '@angular-eslint/component-selector': DISABLED,
         '@angular-eslint/directive-selector': SEVERITY,
+        '@angular-eslint/no-lifecycle-call': DISABLED,
+        '@angular-eslint/prefer-on-push-component-change-detection': DISABLED,
+        '@angular-eslint/use-component-selector': DISABLED,
+        '@typescript-eslint/explicit-member-accessibility': DISABLED,
+        '@typescript-eslint/no-explicit-any': DISABLED,
+        '@typescript-eslint/no-magic-numbers': DISABLED,
+        '@typescript-eslint/no-non-null-assertion': DISABLED,
+        'dot-notation': DISABLED,
+        'guard-for-in': DISABLED,
+        'jsdoc/require-jsdoc': DISABLED,
+        'line-comment-position': DISABLED,
+        'no-console': DISABLED,
+        'no-multiple-empty-lines': [
+          SEVERITY,
+          { max: 1 },
+        ],
+        'no-underscore-dangle': DISABLED,
+        'padded-blocks': [
+          SEVERITY,
+          'never',
+        ],
       },
     },
   ],
