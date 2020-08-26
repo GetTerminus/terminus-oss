e1ac5d164e3c6d6995ff357fa949989066e651e0
Benjamin Charity <ben@benjamincharity.com>
2020-08-26 16:43:35 -0400

    chore(Paginator): initial stories

---
+++ libs/ui/paginator/src/lib/paginator.stories.ts
@@ -0,0 +1,156 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { action } from '@storybook/addon-actions';
+import {
+  boolean,
+  number,
+  select,
+  text,
+  withKnobs,
+} from '@storybook/addon-knobs';
+
+import { tsButtonThemes } from '@terminus/ui-button';
+import {
+  TsPaginatorComponent,
+  TsPaginatorModule,
+} from '@terminus/ui-paginator';
+
+export default {
+  title: 'Components/Navigation/Paginator',
+  decorators: [withKnobs],
+};
+
+const MODULE_METADATA = {
+  imports: [
+    BrowserAnimationsModule,
+    TsPaginatorModule,
+  ],
+};
+
+export const basic = () => ({
+  moduleMetadata: MODULE_METADATA,
+  component: TsPaginatorComponent,
+  template: `
+    <ts-paginator
+      [theme]="theme"
+      [totalRecords]="recordCount"
+      (previousPageClicked)="previousPage($event)"
+      (nextPageClicked)="nextPage($event)"
+      (firstPageClicked)="firstPage($event)"
+      (lastPageClicked)="lastPage($event)"
+      (recordsPerPageChange)="recordsPerPageChange($event)"
+    ></ts-paginator>
+  `,
+  props: {
+    recordCount: number('Record count', 47),
+    theme: select('Theme', tsButtonThemes, 'secondary'),
+    firstPage: action('First page'),
+    previousPage: action('Previous page'),
+    nextPage: action('Next page'),
+    lastPage: action('Last page'),
+    recordsPerPageChange: action('Records per-page changed'),
+  },
+});
+// basic.properties = {
+//   actions: { disabled: true },
+// };
+
+export const recordCountMaximum = () => ({
+  moduleMetadata: MODULE_METADATA,
+  component: TsPaginatorComponent,
+  template: `
+    <ts-paginator
+      [totalRecords]="recordCount"
+      [maxPreferredRecords]="maxPreferredRecords"
+      [recordCountTooHighMessage]="recordCountTooHighMessage"
+    ></ts-paginator>
+  `,
+  props: {
+    recordCount: number('Record count', 130),
+    recordCountTooHighMessage: text(
+      'Message when record count is too high',
+      `That is a lot of results! Try refining your filters for better results.`,
+    ),
+    maxPreferredRecords: number('Maximum preferred records', 100),
+  },
+});
+
+export const baseZeroOrOne = () => ({
+  moduleMetadata: MODULE_METADATA,
+  component: TsPaginatorComponent,
+  template: `
+    <ts-paginator
+      [totalRecords]="recordCount"
+      [isZeroBased]="isZeroBased"
+    ></ts-paginator>
+  `,
+  props: {
+    recordCount: number('Record count', 82),
+    isZeroBased: boolean('Zero based', true),
+  },
+});
+
+export const simpleMode = () => ({
+  moduleMetadata: MODULE_METADATA,
+  component: TsPaginatorComponent,
+  template: `
+    <ts-paginator
+      [totalRecords]="recordCount"
+      [isSimpleMode]="isSimpleMode"
+    ></ts-paginator>
+  `,
+  props: {
+    recordCount: number('Record count', 82),
+    isSimpleMode: boolean('Simple mode', true),
+  },
+});
+
+export const noRecordsPerPageSelector = () => ({
+  moduleMetadata: MODULE_METADATA,
+  component: TsPaginatorComponent,
+  template: `
+    <ts-paginator
+      [totalRecords]="recordCount"
+      [showRecordsPerPageSelector]="showRecordsPerPageSelector"
+    ></ts-paginator>
+  `,
+  props: {
+    recordCount: number('Record count', 82),
+    showRecordsPerPageSelector: boolean('Show the Records Per Page menu', false),
+  },
+});
+
+export const setInitialPage = () => ({
+  moduleMetadata: MODULE_METADATA,
+  component: TsPaginatorComponent,
+  template: `
+    <ts-paginator
+      [totalRecords]="recordCount"
+      [currentPageIndex]="currentPageIndex"
+    ></ts-paginator>
+  `,
+  props: {
+    recordCount: number('Record count', 82),
+    currentPageIndex: number('Current page index', 3),
+  },
+});
+
+export const customTooltips = () => ({
+  moduleMetadata: MODULE_METADATA,
+  component: TsPaginatorComponent,
+  template: `
+    <ts-paginator
+      [totalRecords]="recordCount"
+      [firstPageTooltip]="firstPageTooltip"
+      [previousPageTooltip]="previousPageTooltip"
+      [nextPageTooltip]="nextPageTooltip"
+      [lastPageTooltip]="lastPageTooltip"
+    ></ts-paginator>
+  `,
+  props: {
+    recordCount: number('Record count', 82),
+    firstPageTooltip: text('First page tooltip', 'My FIRST page tooltip!'),
+    nextPageTooltip: text('Next page tooltip', 'My NEXT page tooltip!'),
+    previousPageTooltip: text('Previous page tooltip', 'My PREVIOUS page tooltip!'),
+    lastPageTooltip: text('Last page tooltip', 'My LAST page tooltip!'),
+  },
+});
