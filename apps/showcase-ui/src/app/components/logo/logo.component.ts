306432cd4878d9c9aa16ca89f9162269ef239b18
Benjamin Charity <ben@benjamincharity.com>
2020-09-14 09:29:09 -0400

    chore(ShowcaseUI): remove showcase ui app

--- apps/showcase-ui/src/app/components/logo/logo.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'demo-logo',
-  templateUrl: './logo.component.html',
-  styleUrls: ['./logo.component.scss'],
-})
-export class LogoComponent {}
