c8768b58c3dc1932ea21a340ffceb03020bfbc99
Benjamin Charity <ben@benjamincharity.com>
2020-09-14 09:29:09 -0400

    chore(ShowcaseUI): remove showcase ui app

--- apps/showcase-ui/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/showcase-ui',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
-};
