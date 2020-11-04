8b74137505c3aa3dd96e3b04c1645892d69963da
Benjamin Charity <ben@benjamincharity.com>
2020-11-04 13:41:59 -0500

    feat(Pipes): abbreviate number pipe: allow trimming of trailing zeros
    ISSUES CLOSED: #429

--- libs/ui/pipes/src/lib/abbreviate-number/abbreviate-number.pipe.ts
+++ libs/ui/pipes/src/lib/abbreviate-number/abbreviate-number.pipe.ts
@@ -5,24 +5,29 @@ import {
 
 import { abbreviateNumber } from '@terminus/fe-utilities';
 
-
 /**
  * The abbreviate number pipe
  *
  * @example
- * {{ 1234 | tsAbbreviateNumber }}
- * {{ 1200 | tsAbbreviateNumber:2 }}
+ * {{ 1234 | tsAbbreviateNumber }}        : 1.2K
+ * {{ 1200 | tsAbbreviateNumber:3 }}      : 1.200K
+ * {{ 1200 | tsAbbreviateNumber:3:false }}: 1.2K
  *
  * <example-url>https://release--5f0ca4e61af3790022cad2fe.chromatic.com/?path=/story/utilities-pipes-number</example-url>
  */
 @Pipe({ name: 'tsAbbreviateNumber' })
 export class TsAbbreviateNumberPipe implements PipeTransform {
-  public transform(value: number | string, decimalPlace = 1): string {
+  /**
+   * Transform the value
+   *
+   * @param value - The value to transform
+   * @param decimalPlace - How many decimal places should exist
+   * @param allowTrailingZeros - Whether trailing zeros should be stripped
+   * @returns The transformed value
+   */
+  public transform(value: number | string, decimalPlace = 1, allowTrailingZeros = true): string {
     // Check for null values to avoid issues during data-binding
-    if (!value) {
-      return '';
-    }
-    return abbreviateNumber(value, decimalPlace);
+    return value ? abbreviateNumber(value, decimalPlace, allowTrailingZeros) : '';
   }
 }
 
