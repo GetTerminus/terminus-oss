30fd4cc2ad0f0380ae9e242815ffbf1f9469b34a
Benjamin Charity <ben@benjamincharity.com>
2020-11-06 10:49:28 -0500

    fix(Card): bump icon package

--- libs/ui/card/schematics/ng-add/index.ts
+++ libs/ui/card/schematics/ng-add/index.ts
@@ -20,7 +20,7 @@ export const ngAdd = () => (tree: Tree, context: SchematicContext): Tree => {
     '@angular/platform-browser: ^9.1.0',
     '@terminus/design-tokens: ^3.1.0',
     '@terminus/fe-utilities: ^1.0.0',
-    '@terminus/ui-icon: ^2.0.5',
+    '@terminus/ui-icon: ^2.1.4',
     '@terminus/ui-utilities: ^1.0.9',
     '@terminus/ui-card: ^2.0.4',
     'date-fns: ^2.14.0',
