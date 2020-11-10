ae3fae6b9ca0098c2110601259dc5e0bfdfe566d
Benjamin Charity <ben@benjamincharity.com>
2020-11-10 14:37:24 -0500

    fix(Scrollbars): correct dependencies, move stories

--- libs/ui/scrollbars/schematics/ng-add/index.ts
+++ libs/ui/scrollbars/schematics/ng-add/index.ts
@@ -11,18 +11,13 @@ import {
 
 export const ngAdd = () => (tree: Tree, context: SchematicContext): Tree => {
   [
-    '@angular/cdk: ^9.2.4',
     '@angular/common: ^9.1.0',
     '@angular/core: ^9.1.0',
-    '@angular/flex-layout: ~9.0.0-beta.29',
-    '@angular/forms: ^9.1.0',
-    '@angular/platform-browser: ^9.1.0',
     '@terminus/design-tokens: ^3.1.0',
     '@terminus/fe-utilities: ^1.0.0',
-    '@terminus/ui-utilities: ^1.0.9',
+    '@terminus/ui-spacing: ^1.1.3',
     '@terminus/ui-scrollbars: ^1.0.10',
     'ngx-perfect-scrollbar: ^9.0.0',
-    'date-fns: ^2.14.0',
   ].map(p => {
     const individualPackage = p.split(':');
     const nodeDependency: NodeDependency = {
