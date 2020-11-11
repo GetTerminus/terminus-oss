58804604027faa7544bcf9c583701bca40eb3721
Benjamin Charity <ben@benjamincharity.com>
2020-11-11 10:12:57 -0500

    fix(Option): add ui-styles to schematic

--- libs/ui/option/schematics/ng-add/index.ts
+++ libs/ui/option/schematics/ng-add/index.ts
@@ -21,6 +21,7 @@ export const ngAdd = () => (tree: Tree, context: SchematicContext): Tree => {
     '@terminus/fe-utilities: ^1.0.0',
     '@terminus/ui-checkbox: ^2.0.6',
     '@terminus/ui-option: ^1.0.10',
+    '@terminus/ui-styles: ^1.1.0',
     '@terminus/ui-utilities: ^1.0.9',
     'date-fns: ^2.14.0',
     'rxjs: ^6.5.0',
