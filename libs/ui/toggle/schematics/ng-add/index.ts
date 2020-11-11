eebe80a46db49e070190e71c65d6b83f49a403b2
Benjamin Charity <ben@benjamincharity.com>
2020-11-11 10:31:53 -0500

    fix(Toggle): add ui-styles to schematic

--- libs/ui/toggle/schematics/ng-add/index.ts
+++ libs/ui/toggle/schematics/ng-add/index.ts
@@ -17,8 +17,9 @@ export const ngAdd = () => (tree: Tree, context: SchematicContext): Tree => {
     '@angular/material: ^9.2.4',
     '@terminus/design-tokens: ^3.1.0',
     '@terminus/fe-utilities: ^1.0.0',
-    '@terminus/ui-utilities: ^1.0.9',
+    '@terminus/ui-styles: ^1.1.0',
     '@terminus/ui-toggle: ^2.0.0',
+    '@terminus/ui-utilities: ^1.0.9',
     'date-fns: ^2.14.0',
     'rxjs: ^6.5.0',
   ].map(p => {
