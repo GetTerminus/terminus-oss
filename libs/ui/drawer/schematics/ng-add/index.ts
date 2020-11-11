ad9037223f4d06f95bf68682e4900adea2ae764f
Benjamin Charity <ben@benjamincharity.com>
2020-11-11 09:55:36 -0500

    fix(Drawer): add ui-styles to schematic

--- libs/ui/drawer/schematics/ng-add/index.ts
+++ libs/ui/drawer/schematics/ng-add/index.ts
@@ -21,6 +21,7 @@ export const ngAdd = () => (tree: Tree, context: SchematicContext): Tree => {
     '@terminus/design-tokens: ^3.1.0',
     '@terminus/fe-utilities: ^1.0.0',
     '@terminus/ui-drawer: ^1.0.10',
+    '@terminus/ui-styles: ^1.1.0',
     '@terminus/ui-utilities: ^1.0.9',
     'date-fns: ^2.14.0',
     'rxjs: ^6.5.0',
