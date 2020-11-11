b590625160d6afb2f6fa53c699c05d45c483d108
Benjamin Charity <ben@benjamincharity.com>
2020-11-11 09:48:10 -0500

    fix(Checkbox): add ui-styles to schematic

--- libs/ui/checkbox/schematics/ng-add/index.ts
+++ libs/ui/checkbox/schematics/ng-add/index.ts
@@ -17,8 +17,9 @@ export const ngAdd = () => (tree: Tree, context: SchematicContext): Tree => {
     '@angular/forms: ^9.1.0',
     '@angular/platform-browser: ^9.1.0',
     '@terminus/design-tokens: ^3.1.0',
-    '@terminus/ui-utilities: ^1.0.9',
     '@terminus/ui-checkbox: ^3.0.0',
+    '@terminus/ui-styles: ^1.1.0',
+    '@terminus/ui-utilities: ^1.0.9',
   ].map(p => {
     const individualPackage = p.split(':');
     const nodeDependency: NodeDependency = {
