fac81052319e5c4b3c42e94b0a760f0b1af18876
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 13:00:28 -0500

    fix(Chart): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/ui/chart/jest.config.js
+++ libs/ui/chart/jest.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-  name: 'ui-chart',
-  preset: '../../../jest.config.js',
+  displayName: 'ui-chart',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/libs/ui/chart',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
-  roots: [
-    './src/lib',
-    '../../../specs/ui-chart',
-  ],
+  roots: ['./src/lib', '../../../specs/ui-chart'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
 };
--- libs/ui/chart/schematics/ng-add/index.ts
+++ libs/ui/chart/schematics/ng-add/index.ts
@@ -11,20 +11,20 @@ import {
 
 export const ngAdd = () => (tree: Tree, context: SchematicContext): Tree => {
   [
-    '@amcharts/amcharts4-geodata: ^4.1.14',
-    '@amcharts/amcharts4: ^4.9.20',
-    '@angular/common: ^9.1.0',
-    '@angular/core: ^9.1.0',
-    '@angular/forms: ^9.1.0',
-    '@angular/platform-browser: ^9.1.0',
+    '@amcharts/amcharts4-geodata: ^4.10.12',
+    '@amcharts/amcharts4: ^4.1.19',
+    '@angular/common: ^11.0.4',
+    '@angular/core: ^11.0.4',
+    '@angular/forms: ^11.0.4',
+    '@angular/platform-browser: ^11.0.4',
     '@terminus/design-tokens: ^3.1.0',
-    '@terminus/fe-utilities: ^1.0.0',
-    '@terminus/ui-chart: ^1.0.13',
-    '@terminus/ui-spacing: ^1.1.3',
-    '@terminus/ui-styles: ^1.1.0',
-    '@terminus/ui-utilities: ^1.0.9',
-    'date-fns: ^2.14.0',
-    'rxjs: ^6.5.0',
+    '@terminus/fe-utilities: ^2.0.0',
+    '@terminus/ui-chart: ^2.0.0',
+    '@terminus/ui-spacing: ^2.0.0',
+    '@terminus/ui-styles: ^2.0.0',
+    '@terminus/ui-utilities: ^2.0.0',
+    'date-fns: ^2.16.1',
+    'rxjs: ^6.6.1',
   ].map(p => {
     const individualPackage = p.split(':');
     const nodeDependency: NodeDependency = {
