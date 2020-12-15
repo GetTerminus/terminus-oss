a78eb9b910eaeba655271d88e9de2e2e525694e1
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 11:27:24 -0500

    fix(ESLintConfig): move to ESLint 7
    BREAKING CHANGE:
    Now requires >= ESLint 7

--- libs/eslint-config-frontend/jest.config.js
+++ libs/eslint-config-frontend/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: 'eslint-config-frontend',
-  preset: '../../jest.config.js',
+  displayName: 'eslint-config-frontend',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/eslint-config-frontend',
+  globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } },
 };
