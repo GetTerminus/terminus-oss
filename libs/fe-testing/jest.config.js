bc6cc9e419e835abe30f3cebc8679ca33c2d8e93
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 12:16:58 -0500

    fix(FETesting): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/fe-testing/jest.config.js
+++ libs/fe-testing/jest.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-  name: 'fe-testing',
-  preset: '../../jest.config.js',
+  displayName: 'fe-testing',
+  preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/libs/fe-testing',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
-  roots: [
-    './src/lib',
-    '../../specs/fe-testing',
-  ],
+  roots: ['./src/lib', '../../specs/fe-testing'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
 };
