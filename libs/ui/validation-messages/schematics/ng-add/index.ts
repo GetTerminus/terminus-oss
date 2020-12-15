96e0d4a8f4e1b0675c5fb6e516c3a70a76a0a5be
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 14:58:40 -0500

    fix(ValidationMessages): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/ui/validation-messages/schematics/ng-add/index.ts
+++ libs/ui/validation-messages/schematics/ng-add/index.ts
@@ -11,17 +11,17 @@ import {
 
 export const ngAdd = () => (tree: Tree, context: SchematicContext): Tree => {
   [
-    '@angular/common: ^9.1.0',
-    '@angular/core: ^9.1.0',
-    '@angular/forms: ^9.1.0',
-    '@angular/platform-browser: ^9.1.0',
+    '@angular/common: ^11.0.4',
+    '@angular/core: ^11.0.4',
+    '@angular/forms: ^11.0.4',
+    '@angular/platform-browser: ^11.0.4',
     '@terminus/design-tokens: ^3.1.0',
-    '@terminus/fe-utilities: ^1.0.0',
-    '@terminus/ui-pipes: ^1.0.9',
-    '@terminus/ui-styles: ^1.1.0',
-    '@terminus/ui-validation-messages: ^1.0.14',
+    '@terminus/fe-utilities: ^2.0.0',
+    '@terminus/ui-pipes: ^2.0.0',
+    '@terminus/ui-styles: ^2.0.0',
+    '@terminus/ui-validation-messages: ^2.0.0',
     'date-fns: ^2.16.1',
-    'rxjs: ^6.5.0',
+    'rxjs: ^6.6.1',
   ].map(p => {
     const individualPackage = p.split(':');
     const nodeDependency: NodeDependency = {
