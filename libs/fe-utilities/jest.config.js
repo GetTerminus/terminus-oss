4f443748e4a74633d0d92637bff0726145ceebc7
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 12:18:38 -0500

    fix(FEUtilities): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/fe-utilities/jest.config.js
+++ libs/fe-utilities/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: 'fe-utilities',
-  preset: '../../jest.config.js',
+  displayName: 'fe-utilities',
+  preset: '../../jest.preset.js',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/fe-utilities',
   snapshotSerializers: [
@@ -8,8 +8,18 @@ module.exports = {
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
-  roots: [
-    './src/lib',
-    '../../specs/fe-utilities',
-  ],
+  roots: ['./src/lib', '../../specs/fe-utilities'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
 };
