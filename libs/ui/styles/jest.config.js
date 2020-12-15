956437546b1630fbce35fbaeadd8ede09eda3335
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 14:50:01 -0500

    fix(Styles): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/ui/styles/jest.config.js
+++ libs/ui/styles/jest.config.js
@@ -1,10 +1,23 @@
 module.exports = {
-  name: 'ui-styles',
-  preset: '../../../jest.config.js',
+  displayName: 'ui-styles',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/libs/ui/styles',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
 };
