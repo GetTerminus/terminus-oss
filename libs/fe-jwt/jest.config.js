57463648fba92a8fc30949c69c28d42860c5bc7c
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 12:15:37 -0500

    fix(FEJwt): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/fe-jwt/jest.config.js
+++ libs/fe-jwt/jest.config.js
@@ -1,13 +1,23 @@
 module.exports = {
-  name: 'fe-jwt',
-  preset: '../../jest.config.js',
+  displayName: 'fe-jwt',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/fe-jwt',
-  roots: [
-    './src/lib',
-    '../../specs/fe-jwt',
-  ],
+  roots: ['./src/lib', '../../specs/fe-jwt'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
 };
