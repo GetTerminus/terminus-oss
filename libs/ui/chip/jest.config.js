c56d2b8e25bc54af05be0c3234fbf788d81fbfd3
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 13:03:27 -0500

    fix(Chip): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/ui/chip/jest.config.js
+++ libs/ui/chip/jest.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-  name: 'ui-chip',
-  preset: '../../../jest.config.js',
+  displayName: 'ui-chip',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/libs/ui/chip',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
-  roots: [
-    './src/lib',
-    '../../../specs/ui-chip',
-  ],
+  roots: ['./src/lib', '../../../specs/ui-chip'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
 };
