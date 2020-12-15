6d8c648e26954f11ca4dbd3b9f5fb1d36c7c0816
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 13:30:49 -0500

    fix(FormField): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/ui/form-field/jest.config.js
+++ libs/ui/form-field/jest.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-  name: 'ui-form-field',
-  preset: '../../../jest.config.js',
+  displayName: 'ui-form-field',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/libs/ui/form-field',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
-  roots: [
-    './src/lib',
-    '../../../specs/ui-form-field',
-  ],
+  roots: ['./src/lib', '../../../specs/ui-form-field'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
 };
