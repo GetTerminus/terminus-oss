c44ab08782288a0ec38d822f78d278041b67d33d
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 14:27:48 -0500

    fix(Menu): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/ui/menu/README.md
+++ libs/ui/menu/README.md
@@ -25,7 +25,7 @@ ng add @terminus/ui-menu
 
 ### CSS imports
 
-In your top level stylesheet, add these imports:
+In your top-level stylesheet, add these imports:
 
 ```css
 @import '~@terminus/design-tokens/css/library-design-tokens.css';
--- libs/ui/menu/jest.config.js
+++ libs/ui/menu/jest.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-  name: 'ui-menu',
-  preset: '../../../jest.config.js',
+  displayName: 'ui-menu',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/libs/ui/menu',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
-  roots: [
-    './src/lib',
-    '../../../specs/ui-menu',
-  ],
+  roots: ['./src/lib', '../../../specs/ui-menu'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
 };
