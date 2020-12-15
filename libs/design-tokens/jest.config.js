75d2378eb605dbb91efc3e61c15e718c69f0c6d3
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 11:25:55 -0500

    chore(DesignTokens): changes to move to NX11

--- libs/design-tokens/jest.config.js
+++ libs/design-tokens/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: 'design-tokens',
-  preset: '../../jest.config.js',
+  displayName: 'design-tokens',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/design-tokens',
+  globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } },
 };
