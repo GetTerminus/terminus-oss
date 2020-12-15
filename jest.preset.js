4b8a5c57f2a69b106ce51ab256525deef80e1205
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 11:21:19 -0500

    chore(Dependencies): base changes for repo to move to NX11

---
+++ jest.preset.js
@@ -0,0 +1,30 @@
+const nxPreset = require('@nrwl/jest/preset');
+module.exports = {
+  ...nxPreset,
+  cacheDirectory: `${__dirname}/.jest-cache`,
+  clearMocks: true,
+  collectCoverageFrom: [
+    `**/*.{ts,js}`,
+    `libs/**/*.ts`,
+    `!**/*.stories.ts`,
+    `!**/stories/*`,
+    `!**/*.module.ts`,
+    `!**/testing/**`,
+    `!**/index.ts`,
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  coverageReporters: ['text', 'json', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  moduleNameMapper: {
+    '^@terminus/ui-(.*)/testing$': `${__dirname}/libs/ui/$1/testing/src/index.ts`,
+    '^@terminus/ui-(.*)$': `${__dirname}/libs/ui/$1/src/index.ts`,
+  },
+  resolver: '@nrwl/jest/plugins/resolver',
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)', '**/jest-mocks/*'],
+  testPathPattern: '^(libs|specs)/**/*.spec.ts',
+  transform: { '^.+\\.(ts|js|html)$': 'ts-jest' },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@ngrx|popper|@terminus/design-tokens)',
+  ],
+};
