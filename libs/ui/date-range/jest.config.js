0a2d44221e23d2d543514c10bf982e68e903ba60
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 13:24:54 -0500

    fix(DateRange): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/ui/date-range/jest.config.js
+++ libs/ui/date-range/jest.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-  name: 'ui-date-range',
-  preset: '../../../jest.config.js',
+  displayName: 'ui-date-range',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/libs/ui/date-range',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
-  roots: [
-    './src/lib',
-    '../../../specs/ui-date-range',
-  ],
+  roots: ['./src/lib', '../../../specs/ui-date-range'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
 };
