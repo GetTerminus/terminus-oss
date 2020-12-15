70f929ff675260f94bf0cac0a7d6db3570950378
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 13:43:17 -0500

    fix(LoadingOverlay): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/ui/loading-overlay/jest.config.js
+++ libs/ui/loading-overlay/jest.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-  name: 'ui-loading-overlay',
-  preset: '../../../jest.config.js',
+  displayName: 'ui-loading-overlay',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/libs/ui/loading-overlay',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
-  roots: [
-    './src/lib',
-    '../../../specs/ui-loading-overlay',
-  ],
+  roots: ['./src/lib', '../../../specs/ui-loading-overlay'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
 };
