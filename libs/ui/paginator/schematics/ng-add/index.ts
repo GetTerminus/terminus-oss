92dc5b7560abbc8ef79937b56661ef3142701abb
Benjamin Charity <ben@benjamincharity.com>
2020-12-15 14:36:27 -0500

    fix(Paginator): upgrade to NG11 & TS4
    BREAKING CHANGE: Now requires NG11+ and TS4+

--- libs/ui/paginator/schematics/ng-add/index.ts
+++ libs/ui/paginator/schematics/ng-add/index.ts
@@ -11,19 +11,19 @@ import {
 
 export const ngAdd = () => (tree: Tree, context: SchematicContext): Tree => {
   [
-    '@angular/common: ^9.1.0',
-    '@angular/core: ^9.1.0',
-    '@angular/forms: ^9.1.0',
-    '@angular/material: ^9.2.4',
-    '@angular/platform-browser: ^9.1.0',
+    '@angular/common: ^11.0.4',
+    '@angular/core: ^11.0.4',
+    '@angular/forms: ^11.0.4',
+    '@angular/material: ^11.0.3',
+    '@angular/platform-browser: ^11.0.4',
     '@terminus/design-tokens: ^3.1.0',
-    '@terminus/fe-utilities: ^1.0.0',
-    '@terminus/ui-paginator: ^3.0.0',
-    '@terminus/ui-styles: ^1.1.0',
-    '@terminus/ui-tooltip: ^1.0.13',
-    '@terminus/ui-utilities: ^1.1.0',
+    '@terminus/fe-utilities: ^2.0.0',
+    '@terminus/ui-paginator: ^4.0.0',
+    '@terminus/ui-styles: ^2.0.0',
+    '@terminus/ui-tooltip: ^2.0.0',
+    '@terminus/ui-utilities: ^2.0.0',
     'date-fns: ^2.16.1',
-    'rxjs: ^6.5.0',
+    'rxjs: ^6.6.1',
   ].map(p => {
     const individualPackage = p.split(':');
     const nodeDependency: NodeDependency = {
