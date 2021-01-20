3a1b01280729478a323a9a339d00fd55f4cce3e3
Benjamin Charity <ben@benjamincharity.com>
2021-01-20 13:08:43 -0500

    chore: update stylelint package and remove temporary rule

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   extends: '@terminus/stylelint-config-frontend',
-  rules: {
-    // TODO: remove once package is released
-    'length-zero-no-unit': [
-      true,
-      {
-        ignore: ['custom-properties'],
-      },
-    ],
-  },
+  rules: {},
 };
