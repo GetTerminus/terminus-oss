256a6b7d51bff559481d7a80f9adf662278e48a7
Benjamin Charity <ben@benjamincharity.com>
2021-01-21 10:36:53 -0500

    feat(Toggle): full style redesign
    No longer dependent on Material. Closes #532

--- libs/ui/toggle/src/lib/ui-toggle.module.ts
+++ libs/ui/toggle/src/lib/ui-toggle.module.ts
@@ -1,18 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { TsToggleComponent } from './toggle/toggle.component';
 
 export * from './toggle/toggle.component';
 
-
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    MatSlideToggleModule,
   ],
   exports: [TsToggleComponent],
   declarations: [TsToggleComponent],
