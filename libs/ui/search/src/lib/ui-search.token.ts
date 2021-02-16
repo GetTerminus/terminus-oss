a340d05801cb83551e2940bdba2fe86964ec3b50
Sergey Bogush <s.bogush1@gmail.com>
2021-02-16 17:54:47 +0200

    feat(Search): Provide config option for Search module (#549)
    Co-authored-by: Wendy <atlwendy@gmail.com>

---
+++ libs/ui/search/src/lib/ui-search.token.ts
@@ -0,0 +1,5 @@
+import { InjectionToken } from '@angular/core';
+
+import { TsSearchDefaultOptions } from './ui-search.module';
+
+export const TS_SEARCH_DEFAULT_OPTIONS = new InjectionToken<TsSearchDefaultOptions>('TS_SEARCH_DEFAULT_OPTIONS');
