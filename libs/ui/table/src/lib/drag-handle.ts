af6d3845be813adbfe30404721b3b4e1878e98d7
Adam R Moss <adam.moss@terminus.com>
2021-08-24 20:53:41 -0400

    feat(account-hub): Cleanup drag-handle

--- libs/ui/table/src/lib/drag-handle.ts
+++ libs/ui/table/src/lib/drag-handle.ts
@@ -8,9 +8,7 @@ import {
   ElementRef,
   HostBinding,
   Inject,
-  OnInit,
   Optional,
-  Renderer2,
   SkipSelf,
 } from '@angular/core';
 
@@ -29,31 +27,21 @@ import {
      useExisting: TsDragHandleDirective,
    }],
  })
-export class TsDragHandleDirective extends CdkDragHandle implements OnInit {
+export class TsDragHandleDirective extends CdkDragHandle {
   constructor(
     public element: ElementRef<HTMLElement>,
-    private elementRef: ElementRef,
-    private renderer: Renderer2,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     @Inject(CDK_DRAG_PARENT) @Optional() @SkipSelf() parentDrag?: any,
   ) {
     super(element, parentDrag);
   }
 
+  @HostBinding('style.cursor')
+  public readonly cursor = 'grab';
+
   @HostBinding('style.display')
-  public display = 'flex';
+  public readonly display = 'flex';
 
   @HostBinding('style.justify-content')
-  public justifyContent = 'space-between';
-
-  public ngOnInit(): void {
-    this.appendGripLines();
-  }
-
-  private appendGripLines(): void {
-    const gripLines = this.renderer.createElement('span');
-    gripLines.classList.add('fas');
-    gripLines.classList.add('fa-grip-lines');
-    this.renderer.appendChild(this.elementRef.nativeElement, gripLines);
-  }
+  public readonly justifyContent = 'space-between';
 }
